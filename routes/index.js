0116d0bc582c9ad77a6234734e7d85d975653265
prati1 <meprati1@gmail.com>
2017-06-28 22:18:06 +0545

    update

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/try', function(req, res, next) {
+  res.render('try');
+});
+
+router.get('/indext', function(req, res, next) {
+  res.render('indext');
+});
+
 module.exports = router;
